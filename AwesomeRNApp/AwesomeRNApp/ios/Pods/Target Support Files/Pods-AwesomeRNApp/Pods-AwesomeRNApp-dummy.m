9efa991fd47ea1f436b0c90deb968e4185c9b5d4
Vineeth Vijayan <vineeth.v@naicoits.com>
2019-04-02 12:16:14 +0530

    ios certs added

---
+++ AwesomeRNApp/AwesomeRNApp/ios/Pods/Target Support Files/Pods-AwesomeRNApp/Pods-AwesomeRNApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomeRNApp : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomeRNApp
+@end
---
+++ AwesomeRNApp/AwesomeRNApp/ios/Pods/Target Support Files/Pods-AwesomeRNAppTests/Pods-AwesomeRNAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomeRNAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomeRNAppTests
+@end
